029e2e48d879aa0d3a3f27024f26936ba07140c2
Segev BenZvi <sybenzvi@icecube.wisc.edu>
2023-10-13 08:27:23 -0400

    Add PNS masses to HDF5 attributes.

--- models/Fornax_2022/ascii/build_h5.py
+++ models/Fornax_2022/ascii/build_h5.py
@@ -9,33 +9,46 @@ p = ArgumentParser(description='Generate HDF5 outputs from Fornax data')
 p.add_argument('progenitor', help='progenitor folder name')
 args = p.parse_args()
 
-outf = h5py.File(f'lum_spec_{args.progenitor}_dat.h5', 'w')
+# Read in list of PNS baryon masses.
+pns_mass = {}
+with open('pns_2022-2D.txt') as f_pns:
+    for line in f_pns:
+        if line.startswith('#'):
+            continue
+        tokens = line.strip().split()
+        pns_mass[tokens[0]] = float(tokens[1])
 
-# Loop over flavor.
-for flavor in tqdm('012'):
-    nufile = os.path.join(args.progenitor, f'nuspec.{flavor}.xg')
-    nutype = f'nu{flavor}'
+with h5py.File(f'lum_spec_{args.progenitor}_dat.h5', 'w') as outf:
+    mass = args.progenitor[:-3] if 'bh' in args.progenitor else args.progenitor
+    outf.attrs['Mpns'] = pns_mass[mass]
 
-    grp = outf.create_group(nutype)
+    # Loop over flavor.
+    for flavor in tqdm('012'):
+        nufile = os.path.join(args.progenitor, f'nuspec.{flavor}.xg')
+        nutype = f'nu{flavor}'
 
-    # Storage for times, fluxes, energy bins, and energy bin widths.
-    times = []
-    g  = [ [] for _ in range(12) ]
-    egroup  = []
-    degroup = []
+        grp = outf.create_group(nutype)
 
-    for (t, E, F) in read_spec(nufile):
-        spec = Spectrum(t, E, F)
-        times.append(spec.t.to_value('s'))
-        egroup.append(list(spec.E.to_value('MeV')))
-        degroup.append(list(spec.dE.to_value('MeV')))
+        # Storage for times, fluxes, energy bins, and energy bin widths.
+        times = []
+        g  = [ [] for _ in range(12) ]
+        egroup  = []
+        degroup = []
 
-        for j, fl in enumerate(spec.F):
-            g[j].append(fl.to_value('1e50 erg/(MeV s)'))
+        for (t, E, F) in read_spec(nufile):
+            spec = Spectrum(t, E, F)
+            times.append(spec.t.to_value('s'))
+            egroup.append(list(spec.E.to_value('MeV')))
+            degroup.append(list(spec.dE.to_value('MeV')))
 
-    # Write attributes and datasets to each flavor group.
-    grp.attrs['time'] = times
-    grp.create_dataset('egroup', data=egroup)
-    grp.create_dataset('degroup', data=degroup)
-    for j in range(12):
-        grp.create_dataset(f'g{j}', data=g[j])
+            for j, fl in enumerate(spec.F):
+                g[j].append(fl.to_value('1e50 erg/(MeV s)'))
+
+        # Write attributes and datasets to each flavor group.
+        grp.attrs['time'] = times
+        grp.create_dataset('egroup', data=egroup)
+        grp.create_dataset('degroup', data=degroup)
+        for j in range(12):
+            grp.create_dataset(f'g{j}', data=g[j])
+
+    outf.close()
